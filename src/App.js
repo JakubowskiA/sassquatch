8a995fa6727a26eaf7f120e2ceaead37be749712
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-14 18:08:26 -0500

    Added 2nd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <body>
-      <header className="App-header">
-        Sassquatch
-      </header>
-      <img src="sasquatch.png"/>
-      <p className="much">Much sass.</p>
-      <p className="very">Very squatch.</p>
-      <p className="wow">Wow.</p>
-      
-      <div>
-      <p className="much">second smaller sassquatch</p>
-      <img src="sasquatch.png"/>
-      
-      <p className="very">Smaller but no less sassy.</p>
-      <p className="wow">Wow.</p>
-      </div>
+        <header className="App-header">Sassquatch</header>
+        <img src="sasquatch.png" />
+        <p className="much">Much sass.</p>
+        <p className="very">Very squatch.</p>
+        <p className="wow">Wow.</p>
+
+        <div>
+          <p className="much">second smaller sassquatch</p>
+          <img src="sasquatch.png" />
+
+          <p className="very">Smaller but no less sassy.</p>
+          <p className="wow">Wow.</p>
+        </div>
       </body>
     </div>
   );
